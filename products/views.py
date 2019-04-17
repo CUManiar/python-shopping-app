638b74641785925e1dd186cd8c0865642be3592e
CUManiar <37377238+CUManiar@users.noreply.github.com>
2019-04-17 09:30:22 +0530

    Update views.py

--- products/views.py
+++ products/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse
 from .models import Product, Offer
 
 
+# View for index page
 def index(request):
     products = Product.objects.all()
     return render(request, 'index.html',
         {'products': products})
 
 
+# View for new page
 def new(request):
     return HttpResponse("Its Chirag's Page!!")
